44aeeeb6e2cc0ef88e8846edc4b819e51e767101
Martín López Paglione <martincholp@hotmail.com>
2018-11-25 15:24:05 -0300

    Comienzo

---
+++ constants.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+'''Constantes utilizadas en el modulo mtn_GUI'''
+
+
+###################################################
+##                                               ##
+##                 ORIENTACIONES                 ##
+##                                               ##
+##  Orientación de distintos elementos, como por ##
+##  ejemplo los slider, para diferenciar un      ##
+##  slider horizontal de uno vertical.           ##
+###################################################
+O_HORIZONTAL = 0
+O_VERTICAL = 1
+
+###################################################
+##                                               ##
+##                 ALINEACIONES                  ##
+##                                               ##
+##  Alineacion del texto en aquellos elementos   ##
+##  que tienen la propiedad align.               ##
+###################################################
+A_LEFT = 0
+A_RIGHT = 1
+A_TOP = 2
+A_BOTTOM = 3
+A_CENTER = 4
+A_TOPLEFT = 5
+A_TOPRIGHT = 6
+A_BOTTOMLEFT = 7
+A_BOTTOMRIGHT = 8
+
---
+++ mtn_GUI.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+#-*- coding: UTF-8 -*-
+import pygame
+from UltraColor import *
+from constants import *
+
+'''
+mtnGUI
+Es un modulo para crear interfaces de usuario graficas usando solo pygame como libreria dependiente.
+version 0.0.1
+por Martin Lopez Paglione
+'''
+
